f846b51949bb65df79472f152cd1044526557af4
AnnaAndreeva22 <113442695+AnnaAndreeva22@users.noreply.github.com>
2023-03-26 13:06:51 +0300

    Add files via upload

---
+++ settings.py
@@ -0,0 +1,5 @@
+key = '400b3a18ed322c87b6537cd451f1cd66f30eb980e1f4872d134ddbd0'
+
+wrong_key = 'yhnsajkhujdyh'
+
+wrong_id = '5c8d8ac2-390b-42b6-afe8-071a7c681dc2'
---
+++ test_petfriend28.py
@@ -0,0 +1,80 @@
+import pytest
+import json
+import requests
+from pydantic import BaseModel
+from settings import key, wrong_key, wrong_id
+
+class GetApiKey(BaseModel):
+    email = str
+    password = str
+
+
+def test_get_api_key():
+
+    url = 'https://petfriends.skillfactory.ru/api/key'
+    headers = {"email": "anna.an2022@inbox.ru", "password": "petfriend22"}
+    response = requests.get(url, headers=headers)
+
+    assert response.status_code == 200
+    result = response.json()
+    assert response.json()
+
+
+class Pet(BaseModel):
+    name = str
+    animal_type = str
+    age = int
+    pet_id = str
+    filter = str
+
+def test_create_pet_simple():
+
+    url = 'https://petfriends.skillfactory.ru/api/create_pet_simple'
+    data = {'name': 'Bobik', 'animal_type': 'rabbit', 'age': 6}
+    headers = {'auth_key': key}
+
+    response = requests.post(url, headers=headers, data=data)
+
+    assert response.status_code == 200
+
+
+def test_create_pet_simple_with_invalid_key():
+
+    url = 'https://petfriends.skillfactory.ru/api/create_pet_simple'
+    data = {'name': 'Bobik', 'animal_type': 'rabbit', 'age': 6}
+    headers = {'auth_key': wrong_key}
+
+    response = requests.post(url, headers=headers, data=data)
+
+    assert response.status_code == 403
+    print('Use correct auth_key')
+
+
+def test_update_pet_with_wrong_id():
+
+    url = 'https://petfriends.skillfactory.ru/api/pets/ + wrong_id'
+    headers = {'auth_key': key}
+    data = {'name': 'Frankie', 'animal_type': 'cat', 'age': 4}
+
+    response = requests.put(url, headers=headers, data=data)
+
+    assert response.status_code == 400
+    print('Use correct id')
+
+
+
+def test_get_list_of_pets():
+
+    url = 'https://petfriends.skillfactory.ru/api/pets'
+    headers = {'auth_key': key}
+    filter = {'filter': "my_pets"}
+
+    response = requests.get(url, headers=headers, params=filter)
+
+
+    assert response.status_code == 200
+    result = response.json()
+    assert len(result['pets']) > 0
+    print('Number of my pets:', len(result['pets']))
+    assert result
+
